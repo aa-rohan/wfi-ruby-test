92f2109f84f70588a14849df8023c9c937afda92
Aarohan <aarohanvibritannia@gmail.com>
2024-06-25 21:19:44 +0545

    feat: implement logic required for assignment

--- notifications.rb
+++ notifications.rb
@@ -12,11 +12,27 @@ module Notifications
       event = event_name.to_s
 
       method_name = "on_#{class_name_prefix}_#{event}"
+
+      define_method method_name.to_sym do |data|
+        message = CONFIG.dig("notifications", class_name_prefix, event)
+
+        expressions = message.scan(/%{{{(.*?)}}}/).flatten
+
+        expressions.each do |expression|
+          message ["%{{{#{expression}}}}"] = evaluate(data, expression)
+        end
+
+        message
+      end
     end
   end
 
   module InstanceMethods
+    def evaluate(data, expression)
+      exp_array = expression.split(".").map(&:to_sym)
 
+      data.dig(*exp_array)
+    end
   end
 
   def self.included(klass)
