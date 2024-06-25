3565b59903e20daad67e461bafa75d9c9ca441a6
Aarohan <aarohanvibritannia@gmail.com>
2024-06-25 20:34:51 +0545

    WIP: create method name dynamically

--- notifications.rb
+++ notifications.rb
@@ -7,7 +7,11 @@ module Notifications
 
   module ClassMethods
     def listens_to(event_name)
+      index = name.index("Listener")
+      class_name_prefix = name.slice(0...index).downcase
+      event = event_name.to_s
 
+      method_name = "on_#{class_name_prefix}_#{event}"
     end
   end
 
